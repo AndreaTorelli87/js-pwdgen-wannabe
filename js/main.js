508d0e9be5f0c20cc11533707ffad6b99783b2d9
Andrea Torelli <gioran326@protonmail.com>
2023-02-21 14:08:29 +0100

    attacca l'asino dove vuole il padrone

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/*
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito23
+*/
+
+const myName = prompt("Qual'è il tuo nome?");
+const mySurname = prompt("Qual'è il tuo cognome?");
+const myFavoriteColor = prompt("Qual'è il tuo colore preferito?");
+
+document.getElementById('password').innerHTML = "nomecognomecolorepreferito23";
